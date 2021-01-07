65f48e54b42474f990fb3dd5f0d558548e260db1
PrathyushPRao <jay@prashy.com>
2021-01-07 16:28:39 +0530

    csv reader added

--- lib/csv.dart
+++ lib/csv.dart
@@ -1,4 +1,66 @@
-import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+
+class TableLayout extends StatefulWidget {
+  @override
+  _TableLayoutState createState() => _TableLayoutState();
+}
+
+class _TableLayoutState extends State<TableLayout> {
+  List<List<dynamic>> data = [];
+
+  loadAsset() async {
+    final myData = await rootBundle.loadString("assets/sales.csv");
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
+
+    data = csvTable;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: () async {
+            await loadAsset();
+            print(data);
+          }),
+      appBar: AppBar(
+        title: Text("Table Layout and CSV"),
+      ),
+      body: SingleChildScrollView(
+        child: Table(
+          columnWidths: {
+            0: FixedColumnWidth(100.0),
+            1: FixedColumnWidth(200.0),
+          },
+          border: TableBorder.all(width: 1.0),
+          children: data.map((item) {
+            return TableRow(
+                children: item.map((row) {
+              return Container(
+                color:
+                    row.toString().contains("NA") ? Colors.red : Colors.green,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    row.toString(),
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              );
+            }).toList());
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+/* import 'dart:io';
 import 'dart:async';
 
 import 'package:path_provider/path_provider.dart';
@@ -104,3 +166,4 @@ class Storage {
     return file.writeAsString('$data');
   }
 }
+ */
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CsvApp(),
+      home: TableLayout(),
     );
   }
 }
