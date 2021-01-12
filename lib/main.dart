d04dee060ba4a267a2a963734a1150ff263677ca
PrathyushPRao <jay@prashy.com>
2021-01-12 20:54:41 +0530

    package update

--- lib/main.dart
+++ lib/main.dart
@@ -35,3 +35,74 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/* import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_file_picker/form_builder_file_picker.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<FormBuilderState> _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FormBuilder FilePicker Example"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              FormBuilderFilePicker(
+                name: "images",
+                decoration: InputDecoration(labelText: "Attachments"),
+                maxFiles: null,
+                previewImages: true,
+                onChanged: (val) => print(val),
+                selector: Row(
+                  children: <Widget>[
+                    Icon(Icons.file_upload),
+                    Text('Upload'),
+                  ],
+                ),
+                onFileLoading: (val) {
+                  print(val);
+                },
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  _formKey.currentState.save();
+                  print(_formKey.currentState.value);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+} */
